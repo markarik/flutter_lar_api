9b328cabe08a9a091e4617c4847a2d8a67ea918b
markarik <kariukimarkg@gmail.com>
2020-06-11 16:05:31 +0300

    relationships

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = ['customer', 'review', 'product_id', 'star'];
+    public function product()
+    {
+        return $this->belongsTo('Product','product_id','id');
+    }
 }
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,20 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Product;
 use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        $users = Product::all()->pluck('id'),
+
+        'product_id' =>$faker->randomElement($users),
+
+
+//        'product_id'=>function(){Product::all()->random();},
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5)
     ];
 });
